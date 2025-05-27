7c868bb733681170269d75c9a459d4220195a1a9
Caio Eduardo Ferreira de Miranda <cefm23@inf.ufpr.br>
2025-05-27 11:47:58 -0300

    Step logic skeleton

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         char keyPressed;
+        CoordinateTuple<Integer, Integer> stepCoordinates;
 
         Game game = new Game(20, 50);
 
@@ -11,6 +12,28 @@ public class Main {
             game.render();
             System.out.print("Input: ");
             keyPressed = input.next().charAt(0);
+
+            switch (keyPressed) {
+                case 'w':
+
+                break;
+                
+                case 'a':
+
+                break;
+                
+                case 's':
+
+                break;
+                
+                case 'd':
+
+                break;
+
+                default:
+
+                break;
+            }
         } while (!game.hasCollided());
 
         input.close();
